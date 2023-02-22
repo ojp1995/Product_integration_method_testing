63ffb942f964e17814dc5bc3e60ccdf8e6b9ca4b
ojp1995 <o.j.phillips@pgr.reading.ac.uk>
2023-02-22 21:47:24 +0000

    adds error comparison and EOC for non-singular function

--- midpoint_method_convvergence_test.m
+++ midpoint_method_convvergence_test.m
@@ -27,17 +27,32 @@ for j = 1: length(N)
 
     n = [a+h/2:h: b - h/2];  % these are the midpoints of each interval, the nodes
 
-    z(j) = midpoint(f, h, n);
+%     z(j) = midpoint(f, h, n);
 
     z_vec(j) = midpoint_vec(f, h, n);
 
     z_PIM(j) = PIM_midpoint(sigma1, m1, sigma2, m2, h, n);
 
+    err_mid(j) = abs( z_vec(j) - true_sol );
 
+    err_PIM(j) = abs( z_PIM(j) - true_sol );
     
     
 end
 
 % TODO: convergence, EOC, more challenging integrals
 
-[z.', z_vec.', z_PIM.']
+
+for j = 1:(length(N) - 1)
+    EOC_mid(j) = log2(err_mid(j+1)/err_mid(j));
+
+    EOC_PIM(j) = log2(err_PIM(j+1)/err_PIM(j));
+
+end
+
+[err_mid.', err_PIM.']
+
+[EOC_mid.' EOC_PIM.']
+
+
+
